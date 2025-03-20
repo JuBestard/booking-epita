b724ab04d91d52a1610001b2f3f585acf8c8c1da
julien.bestard <julien.bestard@epita.fr>
2025-03-20 16:15:37 +0100

    feat: credit card number masking

--- app/src/main/java/dev/xdbe/booking/model/booking/CreditCardConverter.java
+++ app/src/main/java/dev/xdbe/booking/model/booking/CreditCardConverter.java
@@ -23,8 +23,13 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
     @Override
     public String convertToEntityAttribute(String dbData) {
         // Step 5: Mask credit card number
-        // Insert your code here to mask credit card number        
-        return dbData;
+        // Insert your code here to mask credit card number
+        String first = dbData.substring(0, 4);
+        String last = dbData.substring(12);
+
+        String stars = "********";
+
+        return first + stars + last;
    }
 
     
